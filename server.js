5b5d7f2c1b71315f541994f62fb6b2a9c6aa7077
Russ <shtooney@yahoo.com>
2019-02-14 11:46:51 -0500

    modified scripts and ports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -59,7 +60,8 @@ app.get('/bad', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000');
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
 });
+
 
